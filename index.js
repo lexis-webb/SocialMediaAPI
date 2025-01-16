a9c339255dddc9cda7b6a508b4482c182da4ed6a
lexis-webb <lexiswebb.design@gmail.com>
2025-01-16 18:51:05 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 4000;
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-app.listen(4000, ()=>{
-    console.log("Server is running");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("Homepage")
 })
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
